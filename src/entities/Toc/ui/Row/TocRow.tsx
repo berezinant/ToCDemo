434c000d5e00c9df777799e1baa4835fae565be7
berezinant <berezinant@gmail.com>
2023-07-03 00:22:22 +0200

    feat: added TocRow component

---
+++ src/entities/Toc/ui/Row/TocRow.tsx
@@ -0,0 +1,46 @@
+import cx from 'classnames';
+import { JSX } from 'react';
+import { Link } from 'react-router-dom';
+import { Icon } from '../../../../shared/ui/Icon';
+import styles from './styles.module.scss';
+
+interface TocRowProps {
+  title: string;
+  isExpanded?: boolean;
+  isActive?: boolean;
+  children?: JSX.Element | JSX.Element[];
+  onClick?: () => void;
+  indent?: number;
+}
+
+export function TocRow({
+  title,
+  isExpanded,
+  isActive,
+  onClick = () => null,
+  children,
+  indent = 0,
+}: TocRowProps): JSX.Element {
+  return (
+    <>
+      <Link
+        to={''}
+        onClick={onClick}
+        className={cx(styles.row, { [styles.expanded]: isExpanded, [styles.active]: isActive })}
+      >
+        <span className={cx(styles.title, { [styles[`indent-${indent}`]]: true })}>
+          {children && (
+            <Icon
+              name="arrow-down"
+              className={cx(styles.icon, { [styles.expanded]: isExpanded, [styles.active]: isActive })}
+            />
+          )}
+          {title}
+        </span>
+      </Link>
+      {children && (
+        <div className={cx(styles.children, { [styles.expanded]: isExpanded })}>{isExpanded && children}</div>
+      )}
+    </>
+  );
+}
--- src/ui/ToCRow/ToCRow.tsx
+++
@@ -1,6 +0,0 @@
-import { JSX } from 'react';
-import classNames from './styles.module.scss';
-
-export function ToCRow(): JSX.Element {
-  return <div className={classNames.ToCRow}>Some text</div>;
-}
