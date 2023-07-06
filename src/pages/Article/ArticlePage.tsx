e00f96c1c17b954bca5db6cec9ddc41d16130223
Антон Березин <berezin_a@tochka.com>
2023-07-06 19:52:51 +0200

    feat: built article page

--- src/pages/Article/ArticlePage.tsx
+++ src/pages/Article/ArticlePage.tsx
@@ -1,16 +1,18 @@
 import { JSX } from 'react';
 import { useParams } from 'react-router-dom';
-import { Layout } from '../../shared/ui';
+import { Layout, Skeleton } from '../../shared/ui';
+import styles from './styles.module.scss';
 
-interface ArticlePageProps {
-  title?: string;
-}
+const htmlSuffixLength = 5;
 
-export function ArticlePage({ title }: ArticlePageProps): JSX.Element {
+export function ArticlePage(): JSX.Element {
   const { name } = useParams();
+  const readableName = name?.replace(/-/g, ' ').slice(0, -1 * htmlSuffixLength) ?? '';
+  const capitalizedReadableName = readableName?.charAt(0).toUpperCase() + readableName?.slice(1);
   return (
     <Layout>
-      <h1>{title || name}</h1>
+      <h1 className={styles.title}>{capitalizedReadableName}</h1>
+      <Skeleton itemsCount={Math.floor(Math.random() * 10) + 2} />
     </Layout>
   );
 }
