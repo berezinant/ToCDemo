6eb2c40ad62bae55396532d33799b0586b7b33c9
berezinant <berezinant@gmail.com>
2023-07-07 15:48:09 +0200

    build: supported for testing library tests

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,14 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  testMatch: ['**/__tests__/**/*.+(ts|tsx|js|jsx)', '**/?(*.)+(spec|test).+(ts|tsx|js|jsx)'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|scss)$': '<rootDir>/style-mock.js',
+  },
+
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  testEnvironment: 'jsdom',
 };
