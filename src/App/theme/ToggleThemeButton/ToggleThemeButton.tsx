325ab7f3240b4d6e33fabea3546562bc0b442ffe
berezinant <berezinant@gmail.com>
2023-07-07 02:17:10 +0200

    feat: added style to theme switcher button

--- src/App/theme/ToggleThemeButton/ToggleThemeButton.tsx
+++ src/App/theme/ToggleThemeButton/ToggleThemeButton.tsx
@@ -1,7 +1,16 @@
 import { JSX } from 'react';
 import { useTheme } from '../useTheme';
+import styles from './styles.module.scss';
 
 export function ToggleThemeButton(): JSX.Element {
-  const { toggleTheme } = useTheme();
-  return <button onClick={toggleTheme}>Toggle theme</button>;
+  const { theme, toggleTheme } = useTheme();
+  return (
+    <button
+      className={styles.button}
+      onClick={toggleTheme}
+      title={`Switch to ${theme === 'light' ? 'dark' : 'light'} mode`}
+    >
+      {theme === 'light' ? '🌚' : '🌞'}
+    </button>
+  );
 }
